d7751710f16d7305068713915dbf7ff551b54509
jocko981 <jovannikolic17@gmail.com>
2022-06-14 20:01:08 +0200

    first commit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -11,8 +11,6 @@ export default function SearchBar() {
 
   const handleSearch = (e) => {
     e.preventDefault()
-
-    // search API with term
     fetchSongs(term)
   }
 
@@ -21,21 +19,23 @@ export default function SearchBar() {
     dispatch({ type: "IS_PENDING" })
 
     try {
-      const res = await fetch("https://jsonplaceholder.typicode.com/users", {
-        mode: "cors", signal: controller.signal
+      const res = await fetch(url, {
+        // mode: "cors",
+        signal: controller.signal
       })
       if (!res.ok) {
         dispatch({ type: "ERROR", payload: res.statusText })
       }
       const data = await res.json()
-      // console.log("res", res);
-      // console.log("data", data);
 
+      let uniqueAlbums = data.results.map(item => item.collectionName)
+      uniqueAlbums = Array.from(new Set(uniqueAlbums)).sort().slice(0, 5)
+
+      // test
       const collectionNames = ['Radiohead: The Alan Cross Guide (Unabridged)', 'Opie & Anthony, June 28, 2012', 'True Stories (Bonus Track Version)', 'True Stories (Bonus Track Version)', 'Instrumentals, Vol. 1 (Remastered)', 'Earth Mountain', 'Lifelike', 'Night Sessions', 'In Rainbows', 'In Rainbows', 'In Rainbows', 'In Rainbows', 'In Rainbows', 'In Rainbows', 'Pablo Honey', 'In Rainbows', 'In Rainbows', 'In Rainbows', 'The Twilight Saga: New Moon (Deluxe Version) [Original Motion Picture Soundtrack]', 'In Rainbows', 'Lullaby Renditions of Radiohead', 'OK Computer', 'A Moon Shaped Pool', 'A Moon Shaped Pool', 'A Moon Shaped Pool', 'A Moon Shaped Pool', 'The Eraser', 'A Moon Shaped Pool', 'Lullaby Renditions of Radiohead', 'A Moon Shaped Pool', 'True Stories, A Film By David Byrne: The Complete Soundtrack', 'A Moon Shaped Pool', 'A Moon Shaped Pool', 'A Moon Shaped Pool', 'A Moon Shaped Pool', 'A Moon Shaped Pool', 'Old News - EP', 'The Bends', 'Lullaby Renditions of Radiohead', 'The King of Limbs', 'Lullaby Renditions of Radiohead', 'The Bends', 'OK Computer', 'Radiohead - Single', 'Strung Out On OK Computer: VSQ Performs Radiohead', 'Lullaby Renditions of Radiohead', 'Lullaby Renditions of Radiohead', 'OK Computer', 'OK Computer', 'Lullaby Renditions of Radiohead']
       const uniqueCollectionNames = Array.from(new Set(collectionNames)).sort().slice(0, 5)
 
-      // console.log("radiohead", uniqueCollectionNames);
-      dispatch({ type: "FETCH_SONGS", payload: uniqueCollectionNames })
+      dispatch({ type: "FETCH_SONGS", payload: uniqueAlbums })
     } catch (err) {
       if (err.name === "AbortError") {
         console.log("the fetch was aborted");
